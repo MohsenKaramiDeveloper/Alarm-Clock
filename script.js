ace5e46ab7e18f68f6b147a0f5986e48211ef6a9
Mohsen Karami <kara0158@algonquinlive.com>
2024-05-26 13:13:11 -0400

    add hour selector

--- script.js
+++ script.js
@@ -6,6 +6,6 @@ const selectMenu = document.querySelectorAll('select');
 for(let i = 23 ; i >=0 ; i--){
     i = i < 10 ?  '0' + i : i;
     let option = `<option value="${i}">${i}</option>`;
-    selectMenu[0].firstChild.insertAdjacentHTML('afterend')
-    console.log(option)
+    selectMenu[0].firstElementChild.insertAdjacentHTML('afterend', option)
+    
 }
