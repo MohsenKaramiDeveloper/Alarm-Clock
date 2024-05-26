2dc72b7c575447e329f50017b75532c44e2b1a6c
Mohsen Karami <kara0158@algonquinlive.com>
2024-05-25 22:55:00 -0400

    change 10 t0 11

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const selectMenu = document.querySelectorAll('select');
 
 
 for(let i = 23 ; i >=0 ; i--){
-    i = i < 10 ?  '0' + i : i;
+    i = i < 11 ?  '0' + i : i;
     let option = `<option value="${i}">${i}</option>`;
     selectMenu[0].firstChild.insertAdjacentHTML('afterend')
     console.log(option)
