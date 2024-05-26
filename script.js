9be0be41db2a3e91007fc19fcd1ba75fafa99b35
Mohsen Karami <kara0158@algonquinlive.com>
2024-05-25 22:12:24 -0400

    let option

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
 // start js
 
 const selectMenu = document.querySelectorAll('select');
- 
+
+
+for(let i = 23 ; i >=0 ; i--){
+    let option = `<option value="${i}">${i}</option>`;
+    console.log(option)
+}
