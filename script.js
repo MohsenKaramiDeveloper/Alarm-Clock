c68812ceb21cb359a6de67df7e4453ea28e55a25
Mohsen Karami <kara0158@algonquinlive.com>
2024-05-30 18:30:29 -0400

    add new Date method

--- script.js
+++ script.js
@@ -22,4 +22,19 @@ for(let i = 59 ; i >=0 ; i--){
     
 }
 
-// javascript part one
+// javascript timeBox
+
+
+ setInterval(() =>{
+    let date = new Date();
+    let h = date.getHours();
+    let m = date.getMinutes();
+    let s = date.getSeconds();
+
+    h = h < 10 ? '0' + h : h ;
+    m = m < 10 ? '0' + m : m ;
+    s = s < 10 ? '0' + s : s ;
+
+    console.log( `${h} : ${m}: ${s}` )
+
+ }, 1000)
