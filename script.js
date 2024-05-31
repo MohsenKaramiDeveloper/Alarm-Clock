1f0877282eab98ba0a3a20eed91bae98912d476d
Mohsen Karami <kara0158@algonquinlive.com>
2024-05-30 21:57:44 -0400

    delete line 47

--- script.js
+++ script.js
@@ -44,11 +44,8 @@ for(let i = 59 ; i >=0 ; i--){
 
     let m = date.getMinutes();
 
-    // add seconds
-    let s = date.getSeconds();
-
     
-
+    let s = date.getSeconds();
     h = h < 10 ? '0' + h : h ;
     m = m < 10 ? '0' + m : m ;
     s = s < 10 ? '0' + s : s ;  
